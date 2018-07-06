30ca09c4b88728df813a71813141b326db8aed5f
Erik Vinicius dos Santos Andreazi <erik@tti1.com.br>
2018-07-06 10:14:40 -0300

    Projeto CaraOuCoroa

---
+++ CaraouCoroa/app/src/main/java/caraoucoroa/andreazierik/cursoandroid/com/caraoucoroa/DetalheActivity.java
@@ -0,0 +1,43 @@
+package caraoucoroa.andreazierik.cursoandroid.com.caraoucoroa;
+
+import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class DetalheActivity extends AppCompatActivity {
+
+    private ImageView imagem;
+    private ImageView botaoVoltar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhe);
+
+        imagem = (ImageView) findViewById(R.id.moedaId);
+        botaoVoltar = (ImageView) findViewById(R.id.botavoVoltarId);
+
+        Bundle extra = getIntent().getExtras();
+        if (extra != null){
+            String opcoEscolida = extra.getString("opcao");
+            if (opcoEscolida.equals("cara")){
+                //imagem cara
+                imagem.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.moeda_cara));
+            }else{
+                //imagem coroa
+                imagem.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.moeda_coroa));
+            }
+        }
+
+        botaoVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DetalheActivity.this, MainActivity.class));
+            }
+        });
+
+    }
+}
---
+++ CaraouCoroa/app/src/main/java/caraoucoroa/andreazierik/cursoandroid/com/caraoucoroa/MainActivity.java
@@ -0,0 +1,42 @@
+package caraoucoroa.andreazierik.cursoandroid.com.caraoucoroa;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView botaoJogar;
+    private String[] opcao = {"cara", "coroa"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoJogar = (ImageView) findViewById(R.id.botaoJogarId);
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Gerar número aleatorio
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt(2);//0,1
+
+                //0 cara - 1 coroa
+                //opcao[numeroAleatorio]
+                Intent intent = new Intent(MainActivity.this, DetalheActivity.class);
+                intent.putExtra("opcao", opcao[numeroAleatorio]);
+
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
